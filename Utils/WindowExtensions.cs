ebaca32be4659b01e8ee7be5c2a115e5cb497d37
Maximiliano Jabase <maxijabase@gmail.com>
2022-11-11 22:21:57 -0300

    document method and add check for older versions

--- Utils/WindowExtensions.cs
+++ Utils/WindowExtensions.cs
@@ -5,9 +5,15 @@ namespace SPCode.Utils;
 
 public static class WindowExtensions
 {
+    /// <summary>
+    /// Applies the user-defined theme to the specified window
+    /// </summary>
+    /// <param name="window">Window that will receive the theme</param>
     public static void ApplyTheme(this Window window)
     {
         var themeName = $"{Program.OptionsObject.Program_Theme}.{Program.OptionsObject.Program_AccentColor}";
+        // Prevent previous versions from loading old "Base" prefix nomenclature
+        themeName = themeName.Replace("Base", string.Empty);
         ThemeManager.Current.ChangeTheme(window, themeName);
     }
 }
